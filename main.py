637244053a9db38dce7194c5de591d5667943a53
Prithwis Das <prithwisdas2017@gmail.com>
2021-12-27 06:28:06 +0530

    update

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import csv
 from csv import writer
-data = requests.get('https://www.politico.com/news/magazine/2021/01/18/trump-presidency-administration-biggest-impact-policy-analysis-451479')
+data = requests.get('https://www.politico.com/news/magazine/2021/01/18/trump-presidency-administration-biggest-impact-policy-analysis-451479').text
 soup = BeautifulSoup(data, 'lxml')
-soupdata = soup.findAll('h3', class_='story-text__heading-medium label').text
+soupdata = soup.findAll('h3', class_='story-text__heading-medium label')
 print(soupdata)
