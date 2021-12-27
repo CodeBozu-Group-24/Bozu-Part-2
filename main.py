6bf3b83a299c95b03ef807b84fc0c3049feaa206
Prithwis Das <prithwisdas2017@gmail.com>
2021-12-27 06:21:53 +0530

    update code

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-#code
+from bs4 import BeautifulSoup
+import requests
+import csv
+from csv import writer
+data = requests.get('https://www.politico.com/news/magazine/2021/01/18/trump-presidency-administration-biggest-impact-policy-analysis-451479')
+soup = BeautifulSoup(data, 'lxml')
