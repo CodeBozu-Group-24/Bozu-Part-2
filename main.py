b15ca3c5a1c018073635385f31fcbc646b709d26
Prithwis Das <prithwisdas2017@gmail.com>
2021-12-27 06:35:11 +0530

    update code

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ import csv
 from csv import writer
 data = requests.get('https://www.politico.com/news/magazine/2021/01/18/trump-presidency-administration-biggest-impact-policy-analysis-451479').text
 soup = BeautifulSoup(data, 'lxml')
-soupdata = soup.findAll('h3', class_='story-text__heading-medium label')
-for elements in soupdata:
+soupdata1 = soup.findAll('h3', class_='story-text__heading-medium label')
+soupdata2 = soup.findAll('h3', class_='story-text__heading-medium')
+for elements in soupdata1:
     info = elements.get_text()
     print(info)    
+for elements in soupdata2:
+    info = elements.get_text()
+    print(info)        
