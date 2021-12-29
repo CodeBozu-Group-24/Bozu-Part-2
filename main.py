917679670f844b2b76d864c52a11897b7f2a41b7
Prithwis Das <prithwisdas2017@gmail.com>
2021-12-29 22:50:24 +0530

    update

--- main.py
+++ main.py
@@ -11,8 +11,6 @@ for elements in soupdata:
     things.append(info)  #work done names
 
 things.remove("Religion in schools")
-print(things)
-
 
 moves = []
 impact = []
@@ -32,6 +30,10 @@ for story in story_text:
             if 'The upshot:' in str(element):
                 upshot.append(element.get_text())
 
+things.append("Religion in schools")
+moves.append("DeVos tweaked a wide range of federal education policies, large and small, to bolster faith-based organizations. She changed regulations, for example, to make it easier for members of religious orders to access federal financial aid and expanded federal Public Service Loan Forgiveness to cover clergy members. And she created new protections for faith-based campus organizations at public universities. At the K-12 education level, DeVos stopped enforcing a policy that had prohibited religious organizations from providing publicly funded services—such as tutoring, technology and counseling—in private schools. And she opened up federal grants for charter schools to religiously affiliated organizations.")
+impact.append("Many religious education groups praised DeVos’ changes, which she often described as effort to expand religious liberty. “Too many misinterpret the ‘separation of church and state’ as an invitation for government to separate people from their faith,” she said.")
+upshot.append("The Biden administration is expected to move quickly to roll back many of DeVos’ education policies, but it’s not yet clear how the incoming administration will approach her various policy tweaks to promote religious organizations.")
 for i in range(len(things)):
     full_details = []
     full_details.append(things[i])
