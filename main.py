9529d58bc3bda26ab16c93fd02e921e19613a1cc
Prithwis Das <prithwisdas2017@gmail.com>
2021-12-31 05:28:31 +0530

    update

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import requests
 import csv
 from csv import writer
 import re
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+#works of trump
 data = requests.get('https://www.politico.com/news/magazine/2021/01/18/trump-presidency-administration-biggest-impact-policy-analysis-451479').text
 soup = BeautifulSoup(data, 'lxml')
 soupdata = soup.findAll('h3', class_='story-text__heading-medium')
@@ -35,6 +37,7 @@ things.append("Religion in schools")
 moves.append("DeVos tweaked a wide range of federal education policies, large and small, to bolster faith-based organizations. She changed regulations, for example, to make it easier for members of religious orders to access federal financial aid and expanded federal Public Service Loan Forgiveness to cover clergy members. And she created new protections for faith-based campus organizations at public universities. At the K-12 education level, DeVos stopped enforcing a policy that had prohibited religious organizations from providing publicly funded services—such as tutoring, technology and counseling—in private schools. And she opened up federal grants for charter schools to religiously affiliated organizations.")
 impact.append("Many religious education groups praised DeVos’ changes, which she often described as effort to expand religious liberty. “Too many misinterpret the ‘separation of church and state’ as an invitation for government to separate people from their faith,” she said.")
 upshot.append("The Biden administration is expected to move quickly to roll back many of DeVos’ education policies, but it’s not yet clear how the incoming administration will approach her various policy tweaks to promote religious organizations.")
+"""
 for i in range(len(things)):
     full_details = []
     full_details.append((things[i].encode("ascii", "ignore")).decode())
@@ -45,6 +48,21 @@ for i in range(len(things)):
         writer_object = writer(f)
         writer_object.writerow(full_details)
         f.close()
+"""
+#sentiment
+pos = []
+sid_obj = SentimentIntensityAnalyzer()
+for sentence in impact:
+    sentiment_dict = sid_obj.polarity_scores(sentence)
+    positivity = sentiment_dict['pos']*100
+    pos.append(positivity)
 
-
-
+for i in range(len(things)):
+    full_details = []
+    full_details.append((things[i].encode("ascii", "ignore")).decode())
+    full_details.append((impact[i].encode("ascii", "ignore")).decode())
+    full_details.append(pos[i])
+    with open('sentiment_pos.csv', 'a') as f:
+        writer_object = writer(f)
+        writer_object.writerow(full_details)
+        f.close()
