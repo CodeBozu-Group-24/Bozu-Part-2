08a61d587cf49d719d003ff8e5609d48324ed82d
Prithwis Das <prithwisdas2017@gmail.com>
2021-12-31 05:45:35 +0530

    update

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ things.append("Religion in schools")
 moves.append("DeVos tweaked a wide range of federal education policies, large and small, to bolster faith-based organizations. She changed regulations, for example, to make it easier for members of religious orders to access federal financial aid and expanded federal Public Service Loan Forgiveness to cover clergy members. And she created new protections for faith-based campus organizations at public universities. At the K-12 education level, DeVos stopped enforcing a policy that had prohibited religious organizations from providing publicly funded services—such as tutoring, technology and counseling—in private schools. And she opened up federal grants for charter schools to religiously affiliated organizations.")
 impact.append("Many religious education groups praised DeVos’ changes, which she often described as effort to expand religious liberty. “Too many misinterpret the ‘separation of church and state’ as an invitation for government to separate people from their faith,” she said.")
 upshot.append("The Biden administration is expected to move quickly to roll back many of DeVos’ education policies, but it’s not yet clear how the incoming administration will approach her various policy tweaks to promote religious organizations.")
-"""
+
 for i in range(len(things)):
     full_details = []
     full_details.append((things[i].encode("ascii", "ignore")).decode())
@@ -48,7 +48,7 @@ for i in range(len(things)):
         writer_object = writer(f)
         writer_object.writerow(full_details)
         f.close()
-"""
+
 #sentiment
 pos = []
 neg = []
@@ -62,7 +62,7 @@ for sentence in impact:
     pos.append(positivity)
     neg.append(negativity)
     neu.append(neutrality)
-'''    
+ 
 for i in range(len(things)):
     full_details = []
     full_details.append((things[i].encode("ascii", "ignore")).decode())
@@ -82,7 +82,7 @@ for i in range(len(things)):
         writer_object = writer(f)
         writer_object.writerow(full_details)
         f.close()  
-'''          
+         
 for i in range(len(things)):
     full_details = []
     full_details.append((things[i].encode("ascii", "ignore")).decode())
